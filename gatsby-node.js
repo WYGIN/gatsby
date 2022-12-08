2df9cae1e35a60b3c3289cfd9b81eba5188cf3e9
WYGIN <107541780+WYGIN@users.noreply.github.com>
2022-12-08 17:07:33 +0530

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,1 +1,13 @@
-
+const path = require(`path`)
+exports.createPages = async ({ graphql , actions }) => {
+  const { createPage } = actions
+  
+  const postTemplate = path.resolve(`src/templates/post.js`)
+  const pageTemplate = path.resolve(`src/templates/page.js`)
+  const categoryTemplate = path.resolve(`src/templates/category.js`)
+  const tagTemplate = path.resolve(`src/templates/tag.js`)
+  
+  const postList = await graphql(`
+    
+`)
+}
