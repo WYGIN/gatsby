a55f65496115a9f86cc4973a2318b96089d214ac
WYGIN <107541780+WYGIN@users.noreply.github.com>
2022-12-08 17:29:28 +0530

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,10 @@
+import { graphql } from "gatsby"
+import { AllPosts } from "./src/graphql/AllPosts"
+import { AllPages } from "./src/graphql/AllPages"
+import { AllCategories } from "./src/graphql/AllCategories"
+import { AllTags } from "./src/graphql/AllTags"
+import { AllAuthors } from "./src/graphql/AllAuthors"
+
 const path = require(`path`)
 exports.createPages = async ({ graphql , actions }) => {
   const { createPage } = actions
@@ -6,6 +13,12 @@ exports.createPages = async ({ graphql , actions }) => {
   const pageTemplate = path.resolve(`src/templates/page.js`)
   const categoryTemplate = path.resolve(`src/templates/category.js`)
   const tagTemplate = path.resolve(`src/templates/tag.js`)
+  const authorTemplate = path.resolve(`src/templ6/author.js`)
   
-  const postList = await graphql()
+  const postList = await graphql(AllPosts)
+  const pageList = await graphql(AllPages)
+  const categoryList = await graphql(AllCategories)
+  const tagList = await graphql(AllTags)
+  const authorList = await graphql(AllAuthors)
+  
 }
