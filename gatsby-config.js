3b86fec06ae03eabeaf309521ccf9d33679e507b
WYGIN <107541780+WYGIN@users.noreply.github.com>
2022-12-10 10:35:09 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,43 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+const { linkResolver } = require("./config/prismic/link-resolver"); 
+  
+ require("dotenv").config({ 
+   path: `.env`, 
+ }); 
+ 
 module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+     "gatsby-plugin-image", 
+     "gatsby-plugin-sitemap", 
+     { 
+       resolve: "gatsby-plugin-manifest", 
+       options: { 
+         icon: "src/images/icon.png", 
+       }, 
+     }, 
+     "gatsby-plugin-sharp", 
+     "gatsby-transformer-sharp", 
+     { 
+       resolve: "gatsby-source-filesystem", 
+       options: { 
+         name: "images", 
+         path: "./src/images/", 
+       }, 
+       __key: "images", 
+     }, 
+     { 
+       resolve: "gatsby-source-prismic", 
+       options: { 
+         repositoryName: process.env.GATSBY_PRISMIC_REPO_NAME, 
+         accessToken: process.env.PRISMIC_ACCESS_TOKEN, 
+         customTypesApiToken: process.env.PRISMIC_CUSTOM_TYPES_API_TOKEN, 
+         linkResolver: (doc) => linkResolver(doc), 
+       }, 
+     }
+  ],
 }
