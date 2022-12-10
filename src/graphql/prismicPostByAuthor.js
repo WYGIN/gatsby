7fe4dfc320ec0f3a6ae3f8e435738f947804a2e4
WYGIN <107541780+WYGIN@users.noreply.github.com>
2022-12-10 15:15:00 +0530

    Update prismicPostByAuthor.js

--- src/graphql/prismicPostByAuthor.js
+++ src/graphql/prismicPostByAuthor.js
@@ -1,1 +1,52 @@
-
+const prismicPostByAuthor = `
+  query($author: String!) {
+    allPrismicPost(filter: { data: { author: { ... on prismicAuthor: { data: { name: { eq: $author } } } } } }) {
+      nodes {
+        uid
+        lastPublicationDate
+        data {
+          title
+          featured {
+            localFile {
+              publicURL
+            }
+          }
+          body {
+            richText
+          }
+          category {
+            ... on prismicCategory {
+              data {
+                label
+                body {
+                  ... on PrismicCategoryBodyCategory {
+                    primary {
+                      level1
+                    }
+                    fields {
+                      level2
+                    }
+                  }
+                }
+              }
+            }
+          }
+          author {
+            ... on prismicAuthor {
+              uid
+              data {
+                name
+                profile {
+                  localFile {
+                    publicURL
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+export default prismicPostByAuthor
