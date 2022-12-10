4fb13c40c510e19c2c31495baf71a6821079e84e
WYGIN <107541780+WYGIN@users.noreply.github.com>
2022-12-10 14:06:40 +0530

    Update allPrismicAuthor.js

--- src/graphql/allPrismicAuthor.js
+++ src/graphql/allPrismicAuthor.js
@@ -1,1 +1,28 @@
-
+const allPrismicAuthor = `
+  {
+    allPrismicAuthor {
+      nodes {
+        uid
+        data {
+          name
+          profile {
+            localFile {
+              publicURL
+            }
+          }
+          bio
+          links {
+            label
+            link {
+              ... on _ExternalLink {
+                url
+                target
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+export default allPrismicAuthor
