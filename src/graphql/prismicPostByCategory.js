2e64ce54bd64f18ddb7bf01bddcbf0ef5d9bd0f1
WYGIN <107541780+WYGIN@users.noreply.github.com>
2022-12-10 15:36:36 +0530

    Update prismicPostByCategory.js

--- src/graphql/prismicPostByCategory.js
+++ src/graphql/prismicPostByCategory.js
@@ -1,1 +1,59 @@
-
+const prismicPostByCategory = `
+  query($CategoryId: String!) {
+    allPrismicPost(filter: { data: { category: { ... on prismicCategory: { id: { eq: $CategoryId } } } } }) {
+      nodes {
+        uid
+        lastPublicationDate
+        data {
+          title
+          featured {
+            localFile {
+              publicURL
+            }
+          }
+          body {
+            richText
+          }
+          author {
+            ... on prismicAuthor {
+              uid
+              data {
+                name
+                profile {
+                  localFile {
+                    publicURL
+                  }
+                }
+              }
+            }
+          }
+          category {
+            ... on prismicCategory {
+              data {
+                label
+                body {
+                  ... on PrismicCategoryBodyCategory {
+                    primary {
+                      level1
+                    }
+                    fields {
+                      level2
+                    }
+                  }
+                }
+              }
+            }
+          }
+          tags {
+            ... on prismicTag {
+              data {
+                label
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+export default prismicPostByCategory
