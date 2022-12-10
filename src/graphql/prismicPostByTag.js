6c00ec896e304320970b10cfabbc15ca0ac1ad04
WYGIN <107541780+WYGIN@users.noreply.github.com>
2022-12-10 15:50:06 +0530

    Update prismicPostByTag.js

--- src/graphql/prismicPostByTag.js
+++ src/graphql/prismicPostByTag.js
@@ -1,1 +1,60 @@
+const prismicPostByTag = `
+  query($tagId: String!) {
+    allPrismicPost(filter: { data: { tags: { elemMatch: { id: { eq: $tagId } } } } }) {
+      nodes {
+        uid
+        lastPublicationDate
+        data {
+          title
+          featured {
+            localFile {
+              publicURL
+            }
+          }
+          body {
+            richText
+          }
+          author {
+            ... on prismicAuthor {
+              uid
+              data {
+                name
+                profile {
+                  localFile {
+                    publicURL
+                  }
+                }
+              }
+            }
+          }
+          category {
+            ... on prismicCategory {
+              data {
+                label
+                body {
+                  ... on PrismicCategoryBodyCategory {
+                    primary {
+                      level1
+                    }
+                    fields {
+                      level2
+                    }
+                  }
+                }
+              }
+            }
+          }
+          tags {
+            ... on prismicTag {
+              data {
+                label
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+export default prismicPostByTag
 
