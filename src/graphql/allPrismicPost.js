6f6925acaecb4f23d25f17df63c1e60b0cc06a2e
WYGIN <107541780+WYGIN@users.noreply.github.com>
2022-12-10 14:45:14 +0530

    Update allPrismicPost.js

--- src/graphql/allPrismicPost.js
+++ src/graphql/allPrismicPost.js
@@ -1,1 +1,63 @@
-
+const allPrismicPost = `
+  {
+    allPrismicPost {
+      nodes {
+        uid
+        lastPublicationDate
+        data {
+          title
+          featured {
+            localFile {
+              publicURL
+            }
+          }
+          body {
+            richText
+          }
+          tags {
+            ... on Tag {
+              
+              data {
+                label
+              }
+            }
+          }
+          author {
+            ... on author {
+              uid
+              data {
+                name
+                bio {
+                  richText
+                }
+                profile {
+                  localFile {
+                    publicURL
+                  }
+                }
+              }
+            }
+          }
+          category {
+            ... on Category {
+              data {
+                label
+                body {
+                  ... on CategoryBodyCategory {
+                    primary {
+                      level1
+                    }
+                    fields {
+                      level2
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+export default allPrismicPost
